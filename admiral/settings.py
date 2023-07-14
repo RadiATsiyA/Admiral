ad242f3497ee3521798785927b951c423fba79eb
radia <akkaunt.lewshi@list.ru>
2023-07-14 17:07:40 +0300

    Добавил фильтрацию объектов

--- admiral/settings.py
+++ admiral/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_filters',
     'objects', 
 ]
 
@@ -129,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
