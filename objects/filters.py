caea5257b8d687c620cec8a04d0082caab624016
radia <akkaunt.lewshi@list.ru>
2023-07-14 17:07:22 +0300

    Добавил фильтрацию объектов

---
+++ objects/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+from .forms import ObjectAdFilterForm
+from .models import ObjectAd
+
+
+class ObjectAdFilter(django_filters.FilterSet):
+    price = django_filters.RangeFilter()
+
+    class Meta:
+        model = ObjectAd
+        fields = ['type', 'price', 'city', 'district', 'ad_type', 'rooms', 'area']
+        form = ObjectAdFilterForm
