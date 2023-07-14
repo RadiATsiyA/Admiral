b2e98922210bc2e4070d61dbb826653072a544a7
radia <akkaunt.lewshi@list.ru>
2023-07-14 17:08:36 +0300

    Модернизирова представление списка с возможностью фильтрации

--- objects/views.py
+++ objects/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
 from .models import ObjectAd
+from .filters import ObjectAdFilter
+import django_filters
 
 
 class ObjectsListView(ListView):
     model = ObjectAd
     template_name = 'objects/list.html'
     context_object_name = "object_ad_list"
+    filterset_class = ObjectAdFilter
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = self.filterset_class(self.request.GET, queryset=queryset)
+        return self.filterset.qs
 
 
 class ObjectDetailView(DetailView):
