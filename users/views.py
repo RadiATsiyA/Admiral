d769e84e9ee4d8b9fe8d8f2987cc01681a89981f
radia <akkaunt.lewshi@list.ru>
2023-07-15 11:28:29 +0300

    начал работу с пользователями

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import User, Client, Agent
 
-# Create your views here.
+
+class AgentDetailView(DetailView):
+    model = User
+    template_name = 'users/agent_detail.html'
+    context_object_name = 'agent_details'
