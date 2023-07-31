395de2c26456ba5da875a17fe2f9e6eb600f5473
radia <akkaunt.lewshi@list.ru>
2023-07-31 12:53:11 +0300

    прост изменения

--- objects/admin.py
+++ objects/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import ObjectAd, ObjectImage, ObjectFeedback
+from .models import ObjectAd, ObjectImage, ObjectFeedback, ApplicationToView
 
 # Register your models here.
 admin.site.register(ObjectAd)
 admin.site.register(ObjectImage)
 admin.site.register(ObjectFeedback)
+admin.site.register(ApplicationToView)
