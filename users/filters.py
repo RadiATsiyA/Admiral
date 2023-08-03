6f0b021b098895f9fd4f76b99d440869da700463
radia <akkaunt.lewshi@list.ru>
2023-08-03 16:08:23 +0300

    добавлена фильрация для агентов

---
+++ users/filters.py
@@ -0,0 +1,22 @@
+import django_filters
+from django import forms
+from .models import Agent, Specialization
+from objects.models import District
+
+
+class AgentFilter(django_filters.FilterSet):
+    district = django_filters.ModelChoiceFilter(
+        queryset=District.objects.all(),
+        widget=forms.Select(attrs={'class': 'filter-select', 'id': 'district'}),
+        label='Район',
+    )
+
+    specialization = django_filters.ModelChoiceFilter(
+        queryset=Specialization.objects.all(),
+        widget=forms.Select(attrs={'class': 'filter-select', 'id': 'district'}),
+        label='Специализация',
+    )
+
+    class Meta:
+        model = Agent
+        fields = ['district', 'specialization']
